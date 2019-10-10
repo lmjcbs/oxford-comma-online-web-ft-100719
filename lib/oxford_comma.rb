a4b6e234f98b3e14a47d2945dfa303bf7923cbdb
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-10 12:22:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,6 @@ require 'pry'
 def oxford_comma(array)
   return array.join('') if array.size == 1
   formatted_array = Array.new
-
   array.each_with_index do |word, i|
     i != array.size - 1 ? formatted_array << "#{word}, " : formatted_array << " and #{word}"
   end
